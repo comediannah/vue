1adcc83e8783e2f1cd09c40196cbfb06bf1b42c8
comediannah <istominjob@gmail.com>
2021-10-04 08:35:26 +0300

    l-5

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,88 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div :class="[$style.wrapper]">
+    <header>
+      <div :class="[$style.title]">Мои расходы</div>
+    </header>
+    <main>
+      <PaymentsDisplay :items="getList()" />
+      <Pagination :currentPage="currentPage" :pagLen="getPaginationLength()" />
+      <p>Сумма расходов: {{ getSum() }}. Число записей: {{ getLength() }}</p>
+      <AddCostButton v-on:clicked="showForm = !showForm" />
+      <a href="/add/payment/Food?value=200">Продукты 200</a>
+      <a href="/add/payment/Transport?value=50">Поездки 50</a>
+      <a href="/add/payment/Entertainment?value=2000">Развлечения 2000</a>
+      <AddPaymentForm @addNewPayment="addNewPayment" v-show="showForm" />
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PaymentsDisplay from "./components/PaymentsDisplay.vue";
+import AddPaymentForm from "./components/AddPaymentForm.vue";
+import AddCostButton from "./components/AddCostButton.vue";
+import Pagination from "./components/Pagination.vue";
+import { globalEventBus } from "./main";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    PaymentsDisplay,
+    AddPaymentForm,
+    AddCostButton,
+    Pagination,
+  },
+  data() {
+    return {
+      showForm: false,
+      currentPage: 1,
+      linesOnPage: 3,
+    };
+  },
+  methods: {
+    addNewPayment(data) {
+      this.$store.commit("addPayment", data);
+    },
+    getList() {
+      return this.$store.getters.getFrame(
+        this.linesOnPage * (this.currentPage - 1),
+        this.linesOnPage * this.currentPage
+      );
+    },
+    getSum() {
+      return this.$store.getters.getSumOfPayments;
+    },
+    getLength() {
+      return this.$store.getters.getLength;
+    },
+    getPaginationLength() {
+      return Math.ceil(this.getLength() / this.linesOnPage);
+    },
+  },
+  created() {
+    this.$store.dispatch({ type: "fetchData" });
+    globalEventBus.$on("paginate", (page) => {
+      if (page === "last") {
+        this.currentPage = this.getPaginationLength();
+      } else {
+        this.currentPage = +page;
+      }
+    });
+    this.showForm =
+      this.$route.name === "addpayment" || this.$route.name === "addvalue";
+    if (this.$route.params.page) this.changePage(+this.$route.params.page);
+  },
+};
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass" module>
+.wrapper
+  margin: 20px
+
+.title
+  font-size: 23pt
+  font-family: 'Helvetica Neue', 'Arial', sans-serif
+
+a
+  color: maroon
+  padding: 0 30px 0 0
 </style>
