dc454ed3db8f1c063ee2d3bbce2b5db2c6332549
comediannah <istominjob@gmail.com>
2021-10-11 14:51:03 +0300

    l-7

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,31 @@
 <template>
-  <div :class="[$style.wrapper]">
-    <header>
-      <div :class="[$style.title]">Мои расходы</div>
-    </header>
-    <main>
-      <PaymentsDisplay :items="getList()" />
-      <Pagination :currentPage="currentPage" :pagLen="getPaginationLength()" />
-      <p>Сумма расходов: {{ getSum() }}. Число записей: {{ getLength() }}</p>
-      <AddCostButton v-on:clicked="showForm = !showForm" />
-      <a href="/add/payment/Food?value=200">Продукты 200</a>
-      <a href="/add/payment/Transport?value=50">Поездки 50</a>
-      <a href="/add/payment/Entertainment?value=2000">Развлечения 2000</a>
-      <AddPaymentForm @addNewPayment="addNewPayment" v-show="showForm" />
-    </main>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" class="small-logo" />
+    <Calculator />
   </div>
 </template>
 
 <script>
-import PaymentsDisplay from "./components/PaymentsDisplay.vue";
-import AddPaymentForm from "./components/AddPaymentForm.vue";
-import AddCostButton from "./components/AddCostButton.vue";
-import Pagination from "./components/Pagination.vue";
-import { globalEventBus } from "./main";
+import Calculator from "./components/Calculator.vue";
 
 export default {
   name: "App",
   components: {
-    PaymentsDisplay,
-    AddPaymentForm,
-    AddCostButton,
-    Pagination,
-  },
-  data() {
-    return {
-      showForm: false,
-      currentPage: 1,
-      linesOnPage: 3,
-    };
-  },
-  methods: {
-    addNewPayment(data) {
-      this.$store.commit("addPayment", data);
-    },
-    getList() {
-      return this.$store.getters.getFrame(
-        this.linesOnPage * (this.currentPage - 1),
-        this.linesOnPage * this.currentPage
-      );
-    },
-    getSum() {
-      return this.$store.getters.getSumOfPayments;
-    },
-    getLength() {
-      return this.$store.getters.getLength;
-    },
-    getPaginationLength() {
-      return Math.ceil(this.getLength() / this.linesOnPage);
-    },
-  },
-  created() {
-    this.$store.dispatch({ type: "fetchData" });
-    globalEventBus.$on("paginate", (page) => {
-      if (page === "last") {
-        this.currentPage = this.getPaginationLength();
-      } else {
-        this.currentPage = +page;
-      }
-    });
-    this.showForm =
-      this.$route.name === "addpayment" || this.$route.name === "addvalue";
-    if (this.$route.params.page) this.changePage(+this.$route.params.page);
-  },
-  beforeDestroy() {
-    globalEventBus.$off();
+    Calculator,
   },
 };
 </script>
 
-<style lang="sass" module>
-.wrapper
-  margin: 20px
+<style lang="sass">
+#app
+  font-family: "Helvetica Neue", Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+  margin-top: 10px
 
-.title
-  font-size: 23pt
-  font-family: 'Helvetica Neue', 'Arial', sans-serif
-
-a
-  color: maroon
-  padding: 0 30px 0 0
+.small-logo
+  width: 100px
+  height: 100px
 </style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,8 @@
-import Vue from 'vue';
-import store from "./store";
-import App from './App.vue';
-import router from './router';
+import Vue from 'vue'
+import App from './App.vue'
 
-Vue.config.productionTip = false;
-
-export const globalEventBus = new Vue();
+Vue.config.productionTip = false
 
 new Vue({
-    store,
-    router,
-    render: h => h(App),
-}).$mount('#app');
+  render: h => h(App),
+}).$mount('#app')
