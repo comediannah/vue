00fbb3106481aacd02ae729115429ec5118270c9
comediannah <istominjob@gmail.com>
2021-10-11 14:54:33 +0300

    l-7

---
+++ vueCalcTesting/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" class="small-logo" />
+    <Calculator />
+  </div>
+</template>
+
+<script>
+import Calculator from "./components/Calculator.vue";
+
+export default {
+  name: "App",
+  components: {
+    Calculator,
+  },
+};
+</script>
+
+<style lang="sass">
+#app
+  font-family: "Helvetica Neue", Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+  margin-top: 10px
+
+.small-logo
+  width: 100px
+  height: 100px
+</style>
